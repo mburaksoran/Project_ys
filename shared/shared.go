228c8c42e137f530ae0e225fc3f4f7c7f35fa674
Mehmet Burak Soran <mburaksoran@gmail.com>
2022-01-07 05:50:55 +0300

    Update shared.go

--- shared/shared.go
+++ shared/shared.go
@@ -1,8 +1,8 @@
 package shared
 
 var Config = configuration{
-	MONGOURL:    "mongodb+srv://Agulhas:Agulhas@cluster0.xa1nz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-	POSTGRESURL: "postgres://cgxpmefm:67S9KutggNXm8rXP-OOs4mu4Jmlqat-2@abul.db.elephantsql.com/cgxpmefm",
+	MONGOURL:    "",
+	POSTGRESURL: "",
 }
 
 type configuration struct {
